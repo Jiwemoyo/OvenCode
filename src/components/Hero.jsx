e26937480dc37c6bdcbf2c0b26535bb92fed7901
Alessandro <jiwemoyo@gmail.com>
2024-10-11 11:56:35 -0400

    content hero hecho en react

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id='hero'>
+      <div className='content-hero'>
+        <h2>Welcome to OvenCode</h2>
+        <p>Aqui va el parrafo de hero pero es tan largo que no quiero escribirlo XD</p>
+      </div>
+      <video controls id='video'>
+        <source />
+        Su navegador no soporta este tipo de videos lo sentimos :C
+      </video>
+    </section>
   )
 }
