28f879de3ab69e59f09f1ab64d5ddd9cba3f7ea0
Aless4ndr0 <jiwemoyo@gmail.com>
2024-10-12 11:00:52 -0500

    section hero responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,23 @@
-import '../assets/styles/header.css'
+import { useState } from 'react';
+import '../assets/styles/header.css';
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header id="header">
       <div className="title">
-      <img src="/ovenCodeLogo.png" alt="OvenCodeLogo" id="header-img" />
-      <h1>Oven Code</h1>
+        <img src="/ovenCodeLogo.png" alt="OvenCodeLogo" id="header-img" />
+        <h1>Oven Code</h1>
       </div>
-      <nav id="nav-bar">
-        <a href="#about" className="nav-link">
-          About
-        </a>
-        <a href="#services" className="nav-link">
-          Service
-        </a>
-        <a href="#contact" className="nav-link">
-          Contat Us
-        </a>
+      <nav id="nav-bar" className={isMenuOpen ? 'active' : ''}>
+        <a href="#about" className="nav-link">About</a>
+        <a href="#services" className="nav-link">Service</a>
+        <a href="#contact" className="nav-link">Contact Us</a>
       </nav>
     </header>
   );
